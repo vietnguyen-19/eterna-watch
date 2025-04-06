6a9453b5704b2aa95dcfcb3eefa04048dc2cdfcc
dacdc123 <122297423+dacdc123@users.noreply.github.com>
2025-04-06 23:55:11 +0700

    pix cưng dữ liệu

--- database/seeders/OrderItemSeeder.php
+++ database/seeders/OrderItemSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\OrderItem;
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class OrderItemSeeder extends Seeder
 {
@@ -13,6 +15,40 @@ class OrderItemSeeder extends Seeder
      */
     public function run(): void
     {
-        OrderItem::factory(50)->create();
+        DB::table('order_items')->insert([
+            [
+                'order_id' => 1,
+                'variant_id' => 5,
+                'quantity' => 2,
+                'unit_price' => 750000,
+                'total_price'=> 1750000,
+
+            ],
+            [
+                'order_id' => 2,
+                'variant_id' => 3,
+                'quantity' => 1,
+                'unit_price' => 2750000,
+                'total_price'=> 4750000,
+
+            ],
+            [
+                'order_id' => 3,
+                'variant_id' => 6,
+                'quantity' => 3,
+                'unit_price' => 3750000,
+                'total_price'=> 5750000,
+
+            ],
+            [
+                'order_id' => 4,
+                'variant_id' => 3,
+                'quantity' => 2,
+                'unit_price' => 1450000,
+                'total_price'=> 4750000,
+
+            ],
+        ]);
+        
     }
 }
--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\Order;
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class OrderSeeder extends Seeder
 {
@@ -13,7 +15,44 @@ class OrderSeeder extends Seeder
      */
     public function run(): void
     {
-        Order::factory(1000)->create();
-        
+        DB::table('orders')->insert([
+            [
+                'order_code'=> 'ORD-2023-01-01-0001',
+                'user_id' => 1,
+                'status' => 'pending',
+                'total_amount'=> '1500000',
+                'shipping_method'=> 'free',
+                'created_at' => Carbon::now()->subDays(3),
+                'updated_at' => Carbon::now()->subDays(3),
+            ],
+            [
+                'order_code'=> 'ORD-2024-02-01-0002',
+                'user_id' => 2,
+                'status' => 'pending',
+                'total_amount'=> '2300000',
+                'shipping_method'=> 'free',
+                'created_at' => Carbon::now()->subDays(3),
+                'updated_at' => Carbon::now()->subDays(3),
+            ],
+            [
+                'order_code'=> 'ORD-2024-04-02-0001',
+                'user_id' => 3,
+                'status' => 'pending',
+                'total_amount'=> '2200000',
+                'shipping_method'=> 'free',
+                'created_at' => Carbon::now()->subDays(3),
+                'updated_at' => Carbon::now()->subDays(3),
+            ],
+            [
+                'order_code'=> 'ORD-2024-03-03-0004',
+                'user_id' => 3,
+                'status' => 'pending',
+                'total_amount'=> '1240000',
+                'shipping_method'=> 'free',
+                'created_at' => Carbon::now()->subDays(3),
+                'updated_at' => Carbon::now()->subDays(3),
+            ],
+
+        ]);        
     }
 }
