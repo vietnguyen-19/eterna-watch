db8a9baeba29ae631c30931492cf52ae43e04998
Your Name <quyenndph46311@fpt.edu.vn>
2025-04-15 09:04:21 +0700

    .

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -12,7 +12,6 @@ class Banner extends Model
 {
     use SoftDeletes;
 
-<<<<<<< HEAD
    protected $fillable = [
     'position',
     'title',
@@ -36,21 +35,6 @@ protected $dates = [
     'updated_at',
 ];
 //];
-=======
-
-    protected $fillable = [
-        'position',
-        'title',
-        'image',
-        'link',
-        'description',
-        'is_active',
-    ];
-    protected $casts = [
-        'is_active' => 'boolean',
-    ];
-
->>>>>>> 2403e04aee35146d96dd9a0271398c19f99e9270
     public static function getByPosition(string $position)
     {
         return static::where('position', $position)
