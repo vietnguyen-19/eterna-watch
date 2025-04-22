d5b67e17f5d5bf95827c596bcbf5a1ac5508a74d
vietnguyen-19 <136040307+vietnguyen-19@users.noreply.github.com>
2025-04-22 11:14:43 +0700

    nút hủy comments

--- resources/views/admin/comments/index-post.blade.php
+++ resources/views/admin/comments/index-post.blade.php
@@ -181,7 +181,7 @@
                                                                             <div class="modal-header">
                                                                                 <h5 class="modal-title">Sửa Bình Luận</h5>
                                                                                 <button type="button" class="close"
-                                                                                    data-dismiss="modal" aria-label="Close">
+                                                                                data-bs-dismiss="modal" aria-label="Close">
                                                                                     <span aria-hidden="true">&times;</span>
                                                                                 </button>
                                                                             </div>
@@ -196,7 +196,7 @@
                                                                             <div class="modal-footer">
                                                                                 <button type="button"
                                                                                     class="btn btn-secondary"
-                                                                                    data-dismiss="modal">Hủy</button>
+                                                                                    data-bs-dismiss="modal">Hủy</button>
                                                                                 <button type="button"
                                                                                     class="btn btn-primary"
                                                                                     id="saveEditComment">Lưu
@@ -216,7 +216,7 @@
                                                                                 <h5 class="modal-title">Trả Lời Bình Luận
                                                                                 </h5>
                                                                                 <button type="button" class="close"
-                                                                                    data-dismiss="modal"
+                                                                                    data-bs-dismiss="modal"
                                                                                     aria-label="Close">
                                                                                     <span aria-hidden="true">&times;</span>
                                                                                 </button>
@@ -236,7 +236,7 @@
                                                                             <div class="modal-footer">
                                                                                 <button type="button"
                                                                                     class="btn btn-secondary"
-                                                                                    data-dismiss="modal">Hủy</button>
+                                                                                    data-bs-dismiss="modal">Hủy</button>
                                                                                 <button type="button"
                                                                                     class="btn btn-primary"
                                                                                     id="saveReplyComment">Gửi Trả
