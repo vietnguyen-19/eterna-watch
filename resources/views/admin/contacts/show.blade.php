c525ab166d28505d86845513ad6a558b01257868
dacdc123 <122297423+dacdc123@users.noreply.github.com>
2025-05-04 15:42:20 +0700

    them moi

--- resources/views/admin/contacts/show.blade.php
+++ resources/views/admin/contacts/show.blade.php
@@ -20,9 +20,6 @@
                             </div>
                         </div>
 
-
-
-                        
                         <div class="card-body">
                             <div class="list-group">
                                 <!-- Hiển thị thông tin liên hệ -->
