755d12554309e25483d8643bb2792b6d97c4aab2
dacdc123 <122297423+dacdc123@users.noreply.github.com>
2025-05-04 14:24:58 +0700

    update

--- resources/views/admin/orders/trash.blade.php
+++ resources/views/admin/orders/trash.blade.php
@@ -149,7 +149,6 @@
         </section>
     @endsection
 
-    {{-- adcjasgdajhdgasd --}}
     @section('style')
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
